ed08bd5a0419ad525f2458efc144298641c81d85
NativAmar <89576789+NativAmar@users.noreply.github.com>
2025-04-09 19:40:46 +0300

    ISearchable

--- src/algorithms/search/BreadthFirstSearch.java
+++ src/algorithms/search/BreadthFirstSearch.java
@@ -1,5 +1,5 @@
 package algorithms.search;
 
 public class BreadthFirstSearch extends ASearchingAlgorithm {
-    
+
 }
--- src/algorithms/search/ISearchable.java
+++ src/algorithms/search/ISearchable.java
@@ -1,4 +1,24 @@
 package algorithms.search;
 
+import java.util.ArrayList;
+
 public interface ISearchable {
+    /**
+     *
+     * @return the start state of the maze
+     */
+    AState getStartState();
+
+    /**
+     *
+     * @return the goal state of the maze
+     */
+    AState getGoalState();
+
+    /**
+     *
+     * @param currState a specific state
+     * @return a list of all the states we can achieve from the specific state
+     */
+    ArrayList<AState> getAllPossibleStates(AState currState);
 }
