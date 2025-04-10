936d8f237da5f394b34f67d4d9571ef331e6bef9
galf03 <galfue@post.bgu.ac.il>
2025-04-10 13:22:25 +0300

    העליתי את כל השינויים לפרויקט

--- src/algorithms/mazeGenerators/Position.java
+++ src/algorithms/mazeGenerators/Position.java
@@ -1,4 +1,31 @@
 package algorithms.mazeGenerators;
 
 public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row,int column){
+        this.row=row;
+        this.column=column;
+    }
+    public int getRowIndex(){
+        return this.row;
+    }
+    public int getColumn(){
+        return this.column;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + row + "," + column + "}";
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (!(other instanceof Position)) return false;
+        Position o = (Position) other;
+        return this.row == o.row && this.column == o.column;
+    }
+
 }
--- src/algorithms/mazeGenerators/SimpleMazeGenerator.java
+++ src/algorithms/mazeGenerators/SimpleMazeGenerator.java
@@ -1,2 +1,17 @@
-package algorithms.mazeGenerators;public class SimpleMazeGenerator {
+package algorithms.mazeGenerators;
+
+public class SimpleMazeGenerator extends AMazeGenerator {
+    @Override
+    public Maze generate(int row,int columns){
+        EmptyMazeGenerator emaze = new EmptyMazeGenerator();
+        Maze maze=emaze.generate(row,columns);
+
+        //להגדיר את כל התאים כ1
+        for(int i=0;i<row;i++){
+            for(int j=0;j<columns;j++){
+                maze.setmaze(i,j,1);
+            }
+        }
+
+    }
 }
