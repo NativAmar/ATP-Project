6276b9a67c33e63477767778a1814fe72a5c0239
galf03 <galfue@post.bgu.ac.il>
2025-04-14 10:47:35 +0300

    docstring of mazeGenerators

--- src/algorithms/mazeGenerators/EmptyMazeGenerator.java
+++ src/algorithms/mazeGenerators/EmptyMazeGenerator.java
@@ -1,17 +1,28 @@
 package algorithms.mazeGenerators;
 
+/**
+ * A maze generator that generates an empty maze with no walls (all cells are open).
+ * Start position is set at (0,0), and goal is at the bottom-right corner.
+ */
 public class EmptyMazeGenerator extends AMazeGenerator {
-    public Maze generate(int row,int columns){
-        Maze emptyMaze=new Maze(row,columns);
-        for(int r=0;r<row;r++){
-            for(int c=0;c<columns;c++){
-                emptyMaze.setmaze(r,c,0);
+
+    /**
+     * Generates an empty maze where all cells are paths (value 0).
+     *
+     * @param row     the number of rows in the maze
+     * @param columns the number of columns in the maze
+     * @return the generated Maze object
+     */
+    public Maze generate(int row, int columns) {
+        Maze emptyMaze = new Maze(row, columns);
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < columns; c++) {
+                emptyMaze.setmaze(r, c, 0);
             }
         }
         emptyMaze.setStartPosition(new Position(0, 0));
         emptyMaze.setGoalPosition(new Position(row - 1, columns - 1));
         return emptyMaze;
     }
-
 
 }
--- src/algorithms/mazeGenerators/SimpleMazeGenerator.java
+++ src/algorithms/mazeGenerators/SimpleMazeGenerator.java
@@ -2,8 +2,21 @@ package algorithms.mazeGenerators;
 
 import java.util.Random;
 
+/**
+ * A basic maze generator that connects a start and goal point with a simple path,
+ * and randomly adds additional paths.
+ */
 public class SimpleMazeGenerator extends AMazeGenerator {
     private Random rand = new Random();
+
+    /**
+     * Generates a simple maze by carving a direct path from start to goal,
+     * and adding random openings.
+     *
+     * @param row     the number of rows
+     * @param columns the number of columns
+     * @return a Maze object
+     */
     @Override
     public Maze generate(int row, int columns) {
         if (row < 2 || columns < 2)
@@ -12,14 +25,12 @@ public class SimpleMazeGenerator extends AMazeGenerator {
         EmptyMazeGenerator emaze = new EmptyMazeGenerator();
         Maze maze = emaze.generate(row, columns);
 
-        // להפוך את כל התאים לקירות
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < columns; j++) {
-                maze.setmaze(i, j, 1);
+                maze.setmaze(i, j, 1); // all walls
             }
         }
 
-        // מיקום התחלה וסיום – בקצוות ולא קרובים מדי
         Position start, goal;
         do {
             start = getRandomEdgePosition(row, columns);
@@ -29,47 +40,59 @@ public class SimpleMazeGenerator extends AMazeGenerator {
         maze.setmaze(start.getRowIndex(), start.getColumnIndex(), 0);
         maze.setmaze(goal.getRowIndex(), goal.getColumnIndex(), 0);
 
-        craveSimplePath(maze,start,goal);
+        craveSimplePath(maze, start, goal);
         int numberOfRandomZeros = (row * columns) / 6;
         addRandomZeros(maze, numberOfRandomZeros);
+
         maze.setStartPosition(start);
         maze.setGoalPosition(goal);
 
         return maze;
     }
-    // בוחר נקודה אקראית על גבולות המבוך
+
+    /**
+     * Selects a random edge cell of the maze.
+     */
     private Position getRandomEdgePosition(int row, int col) {
         Random rand = new Random();
-        int wall = rand.nextInt(4); // 0=top, 1=right, 2=bottom, 3=left
+        int wall = rand.nextInt(4);
         switch (wall) {
-            case 0: return new Position(0, rand.nextInt(col));        // עליון
-            case 1: return new Position(rand.nextInt(row), col - 1);  // ימין
-            case 2: return new Position(row - 1, rand.nextInt(col));  // תחתון
-            case 3: return new Position(rand.nextInt(row), 0);        // שמאל
+            case 0: return new Position(0, rand.nextInt(col));
+            case 1: return new Position(rand.nextInt(row), col - 1);
+            case 2: return new Position(row - 1, rand.nextInt(col));
+            case 3: return new Position(rand.nextInt(row), 0);
             default: throw new IllegalArgumentException("Invalid wall index");
         }
     }
 
-    // מחשב מרחק מנח (Manhattan distance) בין 2 נקודות
+    /**
+     * Returns the Manhattan distance between two positions.
+     */
     private int getManhattanDistance(Position p1, Position p2) {
         return Math.abs(p1.getRowIndex() - p2.getRowIndex()) +
                 Math.abs(p1.getColumnIndex() - p2.getColumnIndex());
     }
 
-    private void craveSimplePath(Maze maze,Position start,Position goal){
-      int currentRow=start.getRowIndex();
-      int currentCol=start.getColumnIndex();
-      maze.setmaze(currentRow, currentCol, 0);
-      while(currentRow!=goal.getRowIndex()){
-          currentRow+=(goal.getRowIndex()>currentRow)?1:-1; // כל עוד התנאי בסוגרים מתקיים עולה ב1 אם לא יורד ב1
-          maze.setmaze(currentRow, currentCol, 0);
-      }
-      while(currentCol!=goal.getColumnIndex()){
-          currentCol+=(goal.getColumnIndex()>currentCol)?1:-1;
-          maze.setmaze(currentRow,currentCol,0);
-      }
-
+    /**
+     * Carves a direct path from start to goal (row-first then column).
+     */
+    private void craveSimplePath(Maze maze, Position start, Position goal) {
+        int currentRow = start.getRowIndex();
+        int currentCol = start.getColumnIndex();
+        maze.setmaze(currentRow, currentCol, 0);
+        while (currentRow != goal.getRowIndex()) {
+            currentRow += (goal.getRowIndex() > currentRow) ? 1 : -1;
+            maze.setmaze(currentRow, currentCol, 0);
+        }
+        while (currentCol != goal.getColumnIndex()) {
+            currentCol += (goal.getColumnIndex() > currentCol) ? 1 : -1;
+            maze.setmaze(currentRow, currentCol, 0);
+        }
     }
+
+    /**
+     * Adds a specified number of random path cells (0s) into the maze.
+     */
     private void addRandomZeros(Maze maze, int count) {
         int rows = maze.getRow();
         int cols = maze.getColumn();
@@ -82,5 +105,6 @@ public class SimpleMazeGenerator extends AMazeGenerator {
                 maze.setmaze(r, c, 0);
                 added++;
             }
-
-}}}
+        }
+    }
+}
